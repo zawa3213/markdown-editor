f9c5b10adf0bb927c836d75240fee2c7eb4cc9a8
zawa3213 <ryo3213.tennis.89@gmail.com>
2026-01-17 08:56:08 +0900

    update

---
+++ src/worker/convert_markdown_worker.ts
@@ -0,0 +1,11 @@
+import * as marked from 'marked'
+
+const worker: Worker = self as any
+
+worker.addEventListener('message', (event) => {
+
+  const text = event.data
+  const html = marked(text)
+  worker.postMessage({ html })
+})
+
